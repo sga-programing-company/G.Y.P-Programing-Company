aaac887c29f1b2ebaf8fdb95d6fab5659c08a813
sga-programing-company <sgaprogramingcompany@gmail.com>
2020-11-26 17:02:19 -0500

    inicio 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h2>holass</h2>
   </React.StrictMode>,
   document.getElementById('root')
 );
